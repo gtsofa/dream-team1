9b4a9f02d019d0cc80b265a78b83b0220609030a
Julius Nyule <jlsnyule22@gmail.com>
2018-01-23 19:58:01 +0000

    add changes

---
+++ app/__init__.py
@@ -0,0 +1,36 @@
+
+
+# app/__init__.py
+
+# third-party imports
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+"""
+# local imports
+from config import app_config
+
+# db variable initialization
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+    
+    return app
+    
+    # temporary route
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+        
+    #return app
+    
+"""
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
---
+++ run.py
@@ -0,0 +1,13 @@
+# run.py
+
+import os
+
+# from app import create_app
+from app import app
+
+# config_name = os.getenv('FLASK_CONFIG')
+# app = create_app(config_name)
+
+if __name__ == '__main__':
+    #app.run()
+    app.run(host='0.0.0.0', port=8080, debug=True) #c9
